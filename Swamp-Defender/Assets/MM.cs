d5439f27ce180043c60ad15143e4257eb731affe
uralsamurai <rmr1241@gmail.com>
2024-01-22 18:43:12 +0500

    version 2

--- Swamp-Defender/Assets/MM.cs
+++ Swamp-Defender/Assets/MM.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public void PlayLevel1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void ExitGame()
